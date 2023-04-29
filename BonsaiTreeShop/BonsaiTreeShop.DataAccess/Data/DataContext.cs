2203ea6090488d3b4bf4b10230596ed2d721e3bc
Mojtabarezaei4 <mojtabarezaei2001@gmail.com>
2023-04-29 13:45:54 +0200

    Style: Make some style adjustment to the home page

--- BonsaiTreeShop/BonsaiTreeShop.DataAccess/Data/DataContext.cs
+++ BonsaiTreeShop/BonsaiTreeShop.DataAccess/Data/DataContext.cs
@@ -18,9 +18,7 @@ namespace BonsaiTreeShop.DataAccess.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
+            
             builder.ApplyConfiguration(new RoleConfiguration());
             builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
             builder.ApplyConfiguration(new ProductConfiguration());
